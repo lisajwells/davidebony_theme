242d19208e09d9a97a0020ab90d885fe701379dd
Lisa Wells <lisawells999@gmail.com>
2016-09-10 17:14:31 -0400

    create single template for guest-artist cpt

--- functions.php
+++ functions.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * Displays footer text
  *
  * @param object post
  * @return void
  */
 
 
-
+// * Displays footer text */
 if ( ! function_exists( 'portfoliopress_footer_meta2' ) ):
 function portfoliopress_footer_meta2( $post ) {
 
@@ -37,7 +36,6 @@ function portfoliopress_footer_meta2( $post ) {
 	} ?>
 
 
-
 	<?php if ( $cat_list ) : ?>
 	<span class="cat-links"><span class="entry-utility-prep entry-utility-prep-cat-links"><?php _e( 'Posted in: ', 'portfolio-press' ); ?></span><?php echo $cat_list; ?></span>
 	<?php endif; ?>
@@ -56,8 +54,6 @@ function portfoliopress_footer_meta2( $post ) {
 <?php }
 endif;
 
-
-
 //Enqueue scripts and styles.
 function davidebony_scripts() {
 	wp_enqueue_style('portfolio-press', get_template_directory_uri() .'/style.css');
@@ -70,3 +66,61 @@ add_action( 'wp_enqueue_scripts', 'davidebony_scripts' );
 
 // Enable shortcodes in text widgets
 add_filter('widget_text','do_shortcode');
+
+// --------------
+// Include custom fields in search results ala https://adambalee.com/search-wordpress-by-custom-fields-without-a-plugin/
+/**
+ * Extend WordPress search to include custom fields
+ *
+ * http://adambalee.com
+ */
+
+/**
+ * Join posts and postmeta tables
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_join
+ */
+function cf_search_join( $join ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    }
+
+    return $join;
+}
+add_filter('posts_join', 'cf_search_join' );
+
+/**
+ * Modify the search query with posts_where
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_where
+ */
+function cf_search_where( $where ) {
+    global $pagenow, $wpdb;
+
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+}
+add_filter( 'posts_where', 'cf_search_where' );
+
+/**
+ * Prevent duplicates
+ *
+ * http://codex.wordpress.org/Plugin_API/Filter_Reference/posts_distinct
+ */
+function cf_search_distinct( $where ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+
+    return $where;
+}
+add_filter( 'posts_distinct', 'cf_search_distinct' );
