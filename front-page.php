a9c526f8c355e7b21ec55c16dbf7757b8c276386
Lisa Wells <lisawells999@gmail.com>
2016-09-14 11:54:31 -0400

    link from front page to latest guest artist

--- front-page.php
+++ front-page.php
@@ -15,23 +15,21 @@ function get_latest_guest_link(){
     $permalink = get_permalink($sorted_posts[0]->ID);
     $title = $sorted_posts[0]->post_title;
     $post = $placeholder;
-    $latest_link_html = 'Latest Link: <a href="'.$permalink.'">'.$title.'</a>';
-    return $latest_link_html;
+    // $latest_link_html = 'Latest Link: <a href="'.$permalink.'">'.$title.'</a>';
+    return $permalink;
+    // return $latest_link_html;
 }
 
 $latest_guest = get_latest_guest_link();
-
 ?>
-
 
     <div class="border-top"></div>
     <div class="border-left"></div>
 
     <div class="quads-container">
-    <p><?php echo $latest_guest; ?></p>
 		<a href="/art-and-writing"><div class="top-left"><p>ART &amp; WRITING</p></div></a>
 		<a href="/film-and-music"><div class="top-right"><p>FILM &amp; MUSIC</p></div></a>
-		<a href="/guest-artist"><div class="bot-left"><p>GUEST ARTIST</p></div></a>
+		<a href="<?php echo $latest_guest; ?>"><div class="bot-left"><p>GUEST ARTIST</p></div></a>
 		<a href="/shop"><div class="bot-right"><p>STOP &amp; SHOP</p></div></a>
     </div>
 
