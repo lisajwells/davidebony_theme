f23a5ee136b1df63746f94a6e9ccfac863658004
Lisa Wells <lisawells999@gmail.com>
2016-09-14 11:49:23 -0400

    begin function to link from front page to latest guest artist

--- front-page.php
+++ front-page.php
@@ -1,9 +1,34 @@
-<?php get_header(); ?>
+<?php get_header();
+
+// https://www.webhostinghero.com/how-to-get-the-most-recent-permalink-in-wordpress/
+function get_latest_guest_link(){
+    global $post;
+    $placeholder = $post;
+    $args = array(
+        'post_type' => 'guest_artist',
+        'numberposts'     => 1,
+        'offset'          => 0,
+        'orderby'         => 'post_date',
+        'order'           => 'DESC',
+        'post_status'     => 'publish' );
+    $sorted_posts = get_posts( $args );
+    $permalink = get_permalink($sorted_posts[0]->ID);
+    $title = $sorted_posts[0]->post_title;
+    $post = $placeholder;
+    $latest_link_html = 'Latest Link: <a href="'.$permalink.'">'.$title.'</a>';
+    return $latest_link_html;
+}
+
+$latest_guest = get_latest_guest_link();
+
+?>
+
 
     <div class="border-top"></div>
     <div class="border-left"></div>
 
     <div class="quads-container">
+    <p><?php echo $latest_guest; ?></p>
 		<a href="/art-and-writing"><div class="top-left"><p>ART &amp; WRITING</p></div></a>
 		<a href="/film-and-music"><div class="top-right"><p>FILM &amp; MUSIC</p></div></a>
 		<a href="/guest-artist"><div class="bot-left"><p>GUEST ARTIST</p></div></a>
