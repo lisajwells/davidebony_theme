240022b8dac1b0e9cd735016e929e2ac071bb77c
Lisa Wells <lisawells999@gmail.com>
2016-09-16 13:53:53 -0400

    pulled from sokhi; then created function for menu classes

--- functions.php
+++ functions.php
@@ -147,32 +147,57 @@ function davidebony_get_latest_guest_link(){
 }
 
 // Mark (highlight) custom post type parent (archive) as active item in Wordpress Navigation
-  add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
+  // add_action('nav_menu_css_class', 'add_current_nav_class', 10, 2 );
 
-    function add_current_nav_class($classes, $item) {
+  //   function add_current_nav_class($classes, $item) {
 
-        // Getting the current post details
-        global $post;
+  //       // Getting the current post details
+  //       global $post;
 
-        // Get post ID, if nothing found set to NULL
-        $id = ( isset( $post->ID ) ? get_the_ID() : NULL );
+  //       // Get post ID, if nothing found set to NULL
+  //       $id = ( isset( $post->ID ) ? get_the_ID() : NULL );
 
-        // Checking if post ID exist...
-        if (isset( $id )){
-            // Getting the post type of the current post
-            $current_post_type = get_post_type_object(get_post_type($post->ID));
-            $current_post_type_slug = $current_post_type->rewrite['slug'];
+  //       // Checking if post ID exist...
+  //       if (isset( $id )){
+  //           // Getting the post type of the current post
+  //           $current_post_type = get_post_type_object(get_post_type($post->ID));
+  //           $current_post_type_slug = $current_post_type->rewrite['slug'];
 
-            // Getting the URL of the menu item
-            $menu_slug = strtolower(trim($item->url));
+  //           // Getting the URL of the menu item
+  //           $menu_slug = strtolower(trim($item->url));
 
-            // If the menu item URL contains the current post types slug add the current-menu-item class
-            if (strpos($menu_slug,$current_post_type_slug) !== false) {
+  //           // If the menu item URL contains the current post types slug add the current-menu-item class
+  //           if (strpos($menu_slug,$current_post_type_slug) !== false) {
 
-               $classes[] = 'current-menu-item';
+  //              $classes[] = 'current-menu-item';
 
-            }
-        }
-        // Return the corrected set of classes to be added to the menu item
+  //           }
+  //       }
+  //       // Return the corrected set of classes to be added to the menu item
+  //       return $classes;
+  //   }
+
+/*
+ * Customize Menu Item Classes
+ * @author Bill Erickson
+ * @link http://www.billerickson.net/customize-which-menu-item-is-marked-active/
+ *
+ * @param array $classes, current menu classes
+ * @param object $item, current menu item
+ * @param object $args, menu arguments
+ * @return array $classes
+ */
+function davidebony_menu_item_classes( $classes, $item, $args ) {
+
+    if( 'primary' !== $args->theme_location )
         return $classes;
-    }
+
+    if( ( get_post_type() == 'post' ) && 'Art Writing' == $item->attr_title )
+        $classes[] = 'current-menu-item';
+
+    if( ( get_post_type() == 'film_music' ) && 'Film Music' == $item->attr_title )
+        $classes[] = 'current-menu-item';
+
+    return array_unique( $classes );
+}
+add_filter( 'nav_menu_css_class', 'davidebony_menu_item_classes', 10, 3 );
